e092602ea583712bed4f211ee3f89656096452c2
salva <atlas200x@gmail.com>
2025-06-29 19:49:22 -0500

    idk

--- BuscarAlumnos.py
+++ BuscarAlumnos.py
@@ -4,19 +4,19 @@ import json
 def lambda_handler(event, context):
     body = json.loads(event['body'])
     tenant_id = body.get('tenant_id')
-    producto_id = body.get('producto_id')
+    alumno_id = body.get('alumno_id')
 
-    if not tenant_id or not producto_id:
+    if not tenant_id or not alumno_id:
         return {
             'statusCode': 400,
             'body': json.dumps("Faltan parámetros")
         }
 
-    table = boto3.resource('dynamodb').Table('t_productos')
+    table = boto3.resource('dynamodb').Table('t_alumnos')
     response = table.get_item(
         Key={
             'tenant_id': tenant_id,
-            'producto_id': producto_id
+            'alumno_id': alumno_id
         }
     )
 
